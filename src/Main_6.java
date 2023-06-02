91263d0150f0145be7188cf54b99962765f14e7c
“Mar3eczek17” <marekgrzesiak.22@gmail.com>
2023-06-02 23:58:08 +0200

    Task no. 6 - WIP

--- src/Main_6.java
+++ src/Main_6.java
@@ -1,16 +1,29 @@
-import java.util.ArrayList;
-
 class Employeee {
-    String name;
-    String jobTitle;
-    int salary;
-    private static final ArrayList<Employeee> employeeeCollection = new ArrayList<Employeee>();
-    public Employeee(String name, String jobTitle, int salary) {
+    private final String name;
+    private final String jobTitle;
+    private double salary;
+
+    public Employeee(String name, String jobTitle, double salary) {
         this.name = name;
         this.jobTitle = jobTitle;
         this.salary = salary;
     }
+    public void raiseSalary(double percentage) {
+        salary = salary + salary*percentage/100;
+    }
+    public void printEmployeeDetails() {
+        System.out.println(name);
+        System.out.println(jobTitle);
+        System.out.println(salary);
+    }
 }
 public class Main_6 {
-    Employeee employeee = new Employeee("Karol", "Technique", 3300);
+    public static void main(String[] args) {
+        Employeee employeee = new Employeee("Franziska Waltraud", "HR Manager", 40000);
+        employeee.printEmployeeDetails();
+
+        employeee.raiseSalary(10);
+
+        employeee.printEmployeeDetails();
+    }
 }

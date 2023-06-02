9e7b067bbde2620db359166b426c42b1547b5d2f
“Mar3eczek17” <marekgrzesiak.22@gmail.com>
2023-06-02 18:44:56 +0200

    Task no 4 - adding

--- src/Main_4.java
+++ src/Main_4.java
@@ -1,2 +1,37 @@
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
 public class Main_4 {
+    public static void main(String[] args) {
+        int r1 = 5;
+        Circle circle = new Circle(r1);
+
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+
+        int r2 = 8;
+        circle.setRadius(r2);
+
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+    }
 }

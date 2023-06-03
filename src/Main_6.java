b001f39ffc91f51d7b946021243180d002a711fd
“Mar3eczek17” <marekgrzesiak.22@gmail.com>
2023-06-03 10:36:09 +0200

    Main_6 - update

--- src/Main_6.java
+++ src/Main_6.java
@@ -8,15 +8,18 @@ class Employeee {
         this.jobTitle = jobTitle;
         this.salary = salary;
     }
+
     public void raiseSalary(double percentage) {
-        salary = salary + salary*percentage/100;
+        salary = salary + salary * percentage / 100;
     }
+
     public void printEmployeeDetails() {
         System.out.println(name);
         System.out.println(jobTitle);
         System.out.println(salary);
     }
 }
+
 public class Main_6 {
     public static void main(String[] args) {
         Employeee employeee = new Employeee("Franziska Waltraud", "HR Manager", 40000);

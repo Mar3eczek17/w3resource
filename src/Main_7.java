48258b98c8921cc429e4579d3780b1a6b29851f7
“Mar3eczek17” <marekgrzesiak.22@gmail.com>
2023-06-03 14:07:18 +0200

    Task no. 7 - done

--- src/Main_7.java
+++ src/Main_7.java
@@ -1,36 +1,12 @@
 import java.util.ArrayList;
 
 class Account {
-    private String name;
-    private String accountNumber;
+    private final String name;
+    private final String accountNumber;
     private double balance;
     public Account(String name, String accountNumber, double balance) {
         this.name = name;
         this.accountNumber = accountNumber;
-        this.balance = balance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
         this.balance = balance;
     }
     public void deposit(double amount) {
@@ -43,24 +19,50 @@ class Account {
         return name + ' ' + accountNumber + ' ' + balance;
     }
 }
-// class called "Bank" with a collection of accounts and methods
-// to add and remove
-// accounts, and to deposit and withdraw money.
+// and to deposit and withdraw money.
 class Bank {
-    private ArrayList<Account> accounts;
+    private final ArrayList<Account> accounts;
     public Bank() {
-        accounts = new ArrayList<Account>();
+        accounts = new ArrayList<>();
     }
     public void addAccount(Account account) {
         accounts.add(account);
     }
-    public void removeAccount(Account account) {
-        accounts.remove(account);
+    public void depositMoney(Account account, double amount) {
+        account.deposit(amount);
+    }
+    public void withdrawMoney(Account account, double amount) {
+        account.withdraw(amount);
+    }
+    public ArrayList<Account> getAccounts() {
+        return accounts;
     }
 }
 public class Main_7 {
     public static void main(String[] args) {
-        Account account1 = new Account("Wojtek", "135843", 5000);
+        Bank bank = new Bank();
+
+        Account account1 = new Account("Peter Irmgard", "C0011", 5000);
+        Account account2 = new Account("Katja Ruedi", "C0121", 4500);
+        Account account3 = new Account("Marcella Gebhard", "C0222", 20000);
+
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+        bank.addAccount(account3);
+
+        ArrayList<Account> accounts = bank.getAccounts();
+
+        for (Account account: accounts) {
+            System.out.println(account.getAccountInfo());
+        }
+
+        System.out.println("\nAfter depositing 1000 into account1:");
+        bank.depositMoney(account1, 1000);
         System.out.println(account1.getAccountInfo());
+        System.out.println("No transaction in account2:");
+        System.out.println(account2.getAccountInfo());
+        System.out.println("After withdrawing 5000 from account3:");
+        bank.withdrawMoney(account3, 5000);
+        System.out.println(account3.getAccountInfo());
     }
 }

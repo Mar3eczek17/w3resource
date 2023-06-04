793595f261a65fbc62232c7de976900ce76a2bb7
“Mar3eczek17” <marekgrzesiak.22@gmail.com>
2023-06-04 11:43:15 +0200

    Task no. 8 - done

---
+++ src/Main_9.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class EmploYee {
+    private String name;
+    private double salary;
+    private Date hireDate;
+    public void yearsOfService(String )
+
+}
+public class Main_9 {
+}
